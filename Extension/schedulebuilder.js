aab3ffb113df53d93becb110633683a1de42ad7b
Brian Moses <bmoses00@stuy.edu>
2021-08-03 22:33:33 -0400

    fixed available options bug

--- Extension/schedulebuilder.js
+++ Extension/schedulebuilder.js
@@ -1,3 +1,4 @@
+const main = document.getElementsByTagName('main')[0];
 (async () => {
     // synchronously get professor data from chrome storage to ensure we have it for observer
     const profs = await new Promise((resolve, reject) =>
@@ -6,32 +7,39 @@
         )
     );
     // observe changes so that we modify the table once it appears
-    new MutationObserver(() => addProfessorRatings(profs))
-        .observe(document.getElementsByTagName('main')[0], { childList: true });
+    new MutationObserver(() => checkUrl(profs))
+        .observe(main, { childList: true });
 })();
 
-function addProfessorRatings(professors) {
-    // the reason we use REGEX matching inside the observer instead in manifest.json
-    //  script is that matching subdirectories in manifest.json is unreliable
+function checkUrl(professors) {
+    //  use REGEX matching inside the observer because matching subdirectories in manifest.json is unreliable
     const url = window.location.href;
     const main_page_pattern = 'https:\/\/stonybrook.collegescheduler.com\/terms\/.*\/(courses(?!tatuses)|options)';
     const class_options_pattern = 'https:\/\/stonybrook.collegescheduler.com\/terms\/.*\/courses\/.*';
 
     const tables = document.getElementsByTagName('table');
 
-    // check if URL is correct and that the table has loaded
+    // check if URL matches the main page and that the table has loaded
     if (url.match(main_page_pattern) && tables.length > 0) {
-        modifyTable(professors, tables, true);
+        modifyTableMainPage(professors, tables);
     }
-    // our first observer will catch when the options page begins loading, but not 
-    // when it finishes loading. To avoid the expensive { subtree: true } observer option,
-    // we create another observer which will disconnect when page finishes loading 
     else if (url.match(class_options_pattern)) {
-        new MutationObserver((mutations, observer) => {
-            observer.disconnect();
-            modifyTable(professors, tables, false);
-        }).observe(document.getElementsByTagName('main')[0].firstChild, { childList: true });
+        modifyTableOptionsPage(professors, tables);
     }
+}
+
+function modifyTableMainPage(professors, tables) {
+    modifyTable(professors, tables, true);
+}
+function modifyTableOptionsPage(professors, tables) {
+    new MutationObserver((mutations, observer) => {
+        observer.disconnect();
+        new MutationObserver((mutations, observer) => {
+            if (document.getElementById('enabled_panel'))
+                modifyTable(professors, tables, false);
+        }).observe(main.firstChild.childNodes.item(2), { childList: true });
+        modifyTable(professors, tables, false);
+    }).observe(main.firstChild, { childList: true });
 }
 
 function modifyTable(professors, tables, isMainPage) {
@@ -46,7 +54,7 @@ function modifyTable(professors, tables, isMainPage) {
             // add the css class that ScheduleBuilder uses
             table_el.classList.add('css-0');
             table_el.innerText = 'Rating';
-            el.firstChild.insertBefore(table_el, profNameNode);
+            el.firstChild.insertBefore(table_el, profNameNode.nextSibling);
         }
         // table body rows
         else {
@@ -56,7 +64,7 @@ function modifyTable(professors, tables, isMainPage) {
             table_el.classList.add('css-7aef91-cellCss');
             // add the professor's rating to the table
             table_el.innerHTML = `<span>${profData.rating}</span>`;
-            el.firstChild.insertBefore(table_el, profNameNode);
+            el.firstChild.insertBefore(table_el, profNameNode.nextSibling);
 
             // there is a popup whose colSpan must be increased to make it not look weird
             el.childNodes.item(2).firstChild.colSpan = 12;
