42c56415dbfd11784c4dd72d0ae469e6ae537999
Brian Moses <bmoses00@stuy.edu>
2021-08-08 20:04:09 -0400

    added to schedule

--- Extension/schedulebuilder.js
+++ Extension/schedulebuilder.js
@@ -1,5 +1,7 @@
 const main = document.getElementsByTagName('main')[0];
 const observerConfig = { childList: true };
+
+
 (async () => {
     // synchronously get professor data from chrome storage to ensure we have it for observer
     const profs = await new Promise((resolve, reject) =>
@@ -12,42 +14,40 @@ const observerConfig = { childList: true };
     observer.observe(main, observerConfig);
 })();
 
+
 function checkUrl(professors) {
     //  use REGEX matching inside the observer because matching subdirectories in manifest.json is unreliable
     const url = window.location.href;
-    const main_page_pattern = 'https:\/\/stonybrook.collegescheduler.com\/terms\/.*\/(courses(?!tatuses)|options|schedules(?!.+)|breaks)';
+    const landing_page_pattern = 'https:\/\/stonybrook.collegescheduler.com\/terms\/.*\/(courses(?!tatuses)|options|schedules(?!.+)|breaks)';
     const class_options_pattern = 'https:\/\/stonybrook.collegescheduler.com\/terms\/.*\/courses\/.*';
+    const schedule_page_pattern = 'https:\/\/stonybrook.collegescheduler.com\/terms\/.*\/schedules\/.+';
 
     const tables = document.getElementsByTagName('table');
 
-    // check if URL matches the main page and that the table has loaded
-    if (url.match(main_page_pattern) && tables.length > 0) 
-        handleMainPageChanges(professors, tables);
-    else if (url.match(class_options_pattern)) 
-        handleOptionsPageChanges(professors, tables);
+    url.match(landing_page_pattern)  ? handleLandingPageChanges(professors, tables)  :
+    url.match(class_options_pattern) ? handleOptionsPageChanges(professors, tables)  :
+    url.match(schedule_page_pattern) ? handleSchedulePageChanges(professors, tables) : ''
 }
 
-function handleMainPageChanges(professors, tables) {
+
+function handleLandingPageChanges(professors, tables) {
     modifyTable(professors, tables, true);
 }
+
 function handleOptionsPageChanges(professors, tables) {
-    // once we've clicked on the page, we need to wait for it to load before modifying its table.
-    // for that, we need another mutation observer. Within that observer we need another to observe
-    // one element which can trigger table reconstruction, and another that opens yet another 
-    // tab which can trigger table reconstruction
-
-    // observe the table for changes. Since the table is destroyed and recreated, we must 
-    // attach this observer to the table every time it is recreated
-    const tableObserver = new MutationObserver(() => modifyTable(professors, tables, false, !!document.getElementById('enabled_panel')));
-
-
+    // observes changes to the table and calls modifyTable() when changes are detected
+    const tableObserver = new MutationObserver(
+        () => modifyTable(professors, tables, false, !!document.getElementById('enabled_panel'))
+    );
+    // whenever the page changes call modifyTable(), and also reattach the table observer since 
+    // the table is destroyed and recreated on page change
     const onPageChange = () => {
         tableObserver.observe(tables[0], observerConfig);
         modifyTable(professors, tables, false, !!document.getElementById('enabled_panel'));
     }
     const pageChangeObserver = new MutationObserver(onPageChange);
-
-
+    // once the page and the table finish loading, disconnect the observer (it was just to wait 
+    // for page load), activate the observer for changes in the page, and call modifyTable()
     const onPageLoad = (mutations, observer) => {
         observer.disconnect();
         pageChangeObserver.observe(main.firstChild.children[2], observerConfig);
@@ -57,15 +57,42 @@ function handleOptionsPageChanges(professors, tables) {
     pageLoadObserver.observe(main.firstChild, observerConfig);
 }
 
+function handleSchedulePageChanges(professors, tables) {
+    // div with the information on SBU classes
+    const days = document.getElementsByClassName('css-54eexc-daysCss')[0].children;
+    // uses the 'days' div to get which class each professor is teaching
+    const getProfsClasses = () => {
+        const profsClasses = {};
+        // we need to loop through all grandchildren of the 'days' element
+        [...days].map(day => {
+            [...day.children].map(SBUclass => {
+                // we use this information to figure out which class which professor is teaching
+                const classInfo = [...SBUclass.firstChild.lastChild.children].slice(-2)[0].innerText.split('\n');
+                profsClasses[classInfo[0]] = classInfo[2];
+            });
+        });
+        return profsClasses;
+    }
+    // given the special behavior of the table in the 'schedule' page, we have a special modifyTable() function
+    modifyTableScheduleTab(professors, tables, getProfsClasses());
+    // whenever the schedule changes, modify the new table
+    const scheduleObserver = new MutationObserver(() => modifyTableScheduleTab(professors, tables, getProfsClasses()));
+
+    // add event listeners to detect when the schedule changes
+    [...days].map(day => {
+        scheduleObserver.observe(day, observerConfig);
+    });
+}
+
+
 function modifyTable(professors, tables, isMainPage = false, isEnabledPanel = false) {
-    // there are different values for index of professor name in table and table width
+    // there are different values for index of professor name and for table width
     // for the different tables on the site
     const profNameIndex = isMainPage ? 6 : isEnabledPanel ? 5 : 4;
     const default_table_width = isEnabledPanel ? 10 : 9;
 
     [...tables[tables.length - 1].children].map(row => {
-        // if this row hasn't been modified before. We check per row because some rows 
-        // may be modified and others not
+        // only add to the row if it hasn't been modified from its default width
         if (row.firstChild.children.length === default_table_width) {
             const profNameNode = row.firstChild.children[profNameIndex];;
             
@@ -105,11 +132,68 @@ function modifyTable(professors, tables, isMainPage = false, isEnabledPanel = fa
     });
 }
 
+function modifyTableScheduleTab(professors, tables, profsClasses) {
+    const default_table_width = 13;
+    [...tables[tables.length - 1].children].map(row => {
+        if (row.firstChild.children.length === default_table_width) {
+            if (row.tagName === 'THEAD') {
+                const rating_col = document.createElement('th');
+                // add the css class that ScheduleBuilder uses
+                rating_col.classList.add('css-0');
+                rating_col.innerText = 'Rating';
+                row.firstChild.insertBefore(rating_col, row.firstChild.children[8]);
+
+                const professor_col = document.createElement('th');
+                // add the css class that ScheduleBuilder uses
+                professor_col.classList.add('css-0');
+                professor_col.innerText = 'Professor';
+                row.firstChild.insertBefore(professor_col, row.firstChild.children[8]);
+            }
+            else if (row.tagName === 'TBODY') {
+                const className = row.firstChild.children[6].innerText + '-' + row.firstChild.children[7].innerText;
+                const profName = profsClasses[className];
+
+                const noData = {
+                    difficulty: "No data",
+                    href: "No data",
+                    num_ratings: "No data",
+                    rating: "No data",
+                    would_take_again: "No data"
+                }
+                let profData = professors[profName];
+                if (profData === undefined || profData.num_ratings === "0") profData = noData;
+
+                const rating_col = document.createElement('th');
+                // add the css class that ScheduleBuilder uses
+                rating_col.classList.add('css-7aef91-cellCss');
+                // add the professor's rating to the table
+                rating_col.innerHTML = `<span>${profData.rating}</span>`;
+                row.firstChild.insertBefore(rating_col, row.firstChild.children[8]);
+
+                const professor_col = document.createElement('th');
+                // add the css class that ScheduleBuilder uses
+                professor_col.classList.add('css-7aef91-cellCss');
+                // add the professor's rating to the table
+                professor_col.innerHTML = `<span>${profName}</span>`;
+                row.firstChild.insertBefore(professor_col, row.firstChild.children[8]);
+
+                // there is a popup whose colSpan must be increased to make it not look weird
+                row.children[2].firstChild.colSpan = 15;
+            }
+            else {
+                row.firstChild.firstChild.colSpan = 14;
+            }
+        }
+    });
+}
+
+
 // converts DOM object -> JS object -> String -> JS object then logs it to ensure 
 // console output doesn't change. Code not original.
 function logDomObject(element) {
     console.log(JSON.parse(JSON.stringify(stringify(element), null, '')));
 }
+
 function stringify(element) {
     let obj = {};
     obj.name = element.localName;
