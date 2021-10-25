46c8da0d2133a6c0d1f7503728c257d2f7a171e6
Brian Moses <bmoses00@stuy.edu>
2021-10-24 21:55:40 -0400

    added improved data retrieval

---
+++ Scrape/getProfsData.js
@@ -0,0 +1,75 @@
+import fetch from 'node-fetch';
+
+(async () => {
+    let professors = await getProfessors(true);
+    await addDetailedInfo(professors);
+    compileInfo(professors);
+    console.log(professors);
+})();
+
+// uses RateMyProf's API to get a map of all professors and their id's
+async function getProfessors(testing = false) {
+    if (testing) 
+        console.log("Test run...");
+    let page = 1;
+    let moreData = false
+    const professors = {};
+    do {
+        let SBU_url = `http://www.ratemyprofessors.com/filter/professor/?&page=${page}&filter=teacherlastname_sort_s+asc&query=*%3A*&queryoption=TEACHER&queryBy=schoolId&sid=971`;
+        const res = await fetch(SBU_url);
+        const data = await res.json();
+        moreData = (data.remaining != 0);
+        for (let professor of data.professors) {
+            professors[professor.tFname + " " + professor.tLname] = {
+                "id" : professor.tid
+            };
+        }
+        console.log("Adding page " + page);
+        page++;
+    } while (moreData && !testing) 
+    console.log("Professor retrieval successful");
+    return professors;
+}
+
+// inputs professor id's found in previous function into RateMyProf's APIs to get all ratings
+async function addDetailedInfo(professors) {
+    const professorURL = "https://www.ratemyprofessors.com/paginate/professors/ratings?tid="
+    let count = 0;
+    for (let professor of Object.keys(professors)) {
+        console.log("Adding professor " + count);
+        count++;
+        let url = professorURL + professors[professor].id;
+        const res = await fetch(url);
+        const professorRatings = (await res.json()).ratings;
+        professors[professor]["ratings"] = []
+        for (let professorRating of professorRatings) {
+            let importantRatingData = {};
+
+            importantRatingData.rating = professorRating.rOverall;
+            importantRatingData.difficulty = professorRating.rEasy;
+            importantRatingData.would_retake = professorRating.rWouldTakeAgain;
+            importantRatingData.date = professorRating.rDate;
+            
+            professors[professor]["ratings"].push(importantRatingData);
+        }
+    }
+}
+
+// computes average difficulty and would retake ratings from the individual ratings
+function compileInfo(professors) {
+    for (let professor of Object.keys(professors)) {
+        // console.log(professors[professor].ratings.length);
+        let difficultySum = 0;
+        let wouldRetake = 0;
+        let wouldNotRetake = 0;
+        for (let rating of professors[professor].ratings) {
+            difficultySum += rating.difficulty;
+            if (rating.would_retake === "Yes") wouldRetake++;
+            if (rating.would_retake === "No" ) wouldNotRetake++;
+        }
+        let num_ratings = professors[professor].ratings.length;
+        professors[professor].difficulty = parseFloat((difficultySum / num_ratings).toFixed(1));
+        professors[professor].would_retake = wouldRetake / (wouldRetake + wouldNotRetake);
+        professors[professor].num_ratings = num_ratings;
+    }
+}
